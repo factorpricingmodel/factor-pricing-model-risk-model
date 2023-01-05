08b365953390152522040e2b9dbf40af59561eae
Gavin Chan <gavincyi@gmail.com>
2023-01-05 09:13:52 +0000

    refactor: update bias function prototype

--- src/fpm_risk_model/accuracy/bias.py
+++ src/fpm_risk_model/accuracy/bias.py
@@ -1,15 +1,16 @@
 from typing import Optional
 
-from numpy import ndarray, sqrt, sum
-from pandas import Series
+from numpy import sqrt, sum
+from pandas import DataFrame, Series
 
 from ..rolling_factor_risk_model import RollingFactorRiskModel
 
 
 def compute_standardized_returns(
-    X: ndarray,
-    weights: ndarray,
-    rolling_risk_model: RollingFactorRiskModel,
+    X: DataFrame,
+    weights: DataFrame,
+    rolling_risk_model: Optional[RollingFactorRiskModel] = None,
+    forecast_vols: Optional[Series] = None,
 ) -> Series:
     """
     Compute the standardized returns given the rolling risk model.
@@ -27,6 +28,8 @@ def compute_standardized_returns(
         Weights of the instruments.
     rolling_risk_model: RollingFactorRiskModel
         The rolling risk model.
+    forecast_vols: Series
+        The forecast volatility.
 
     Returns
     -------
@@ -34,20 +37,31 @@ def compute_standardized_returns(
         A timeseries of standardized returns.
     """
     b_t = Series()
+    instruments = weights.columns
     for index, risk_model in rolling_risk_model.items():
         index_weights = weights.loc[index, :]
-        returns = sum(X.loc[index, :] * index_weights)
-        vol = sqrt((risk_model.cov().fillna(0.0) @ index_weights) @ index_weights)
+        returns = sum(X.loc[index, instruments] * index_weights)
+        if forecast_vols is not None:
+            vol = forecast_vols.loc[index]
+        else:
+            cov = (
+                risk_model.cov()
+                .reindex(index=instruments, columns=instruments)
+                .fillna(0.0)
+                .values
+            )
+            vol = sqrt((cov @ index_weights) @ index_weights)
         b_t[index] = returns / vol
 
     return b_t
 
 
 def compute_bias_statistics(
-    X: ndarray,
-    weights: ndarray,
-    rolling_risk_model: RollingFactorRiskModel,
+    X: DataFrame,
+    weights: DataFrame,
     rolling_timeframe: int,
+    rolling_risk_model: Optional[RollingFactorRiskModel] = None,
+    forecast_vols: Optional[Series] = None,
     min_periods: Optional[int] = None,
 ) -> Series:
     """
@@ -69,6 +83,8 @@ def compute_bias_statistics(
         The instrument forecast returns.
     weights: ndarray
         Weights of the instruments.
+    forecast_vols: Optional[Series]
+        The forecast volatility.
     rolling_risk_model: RollingFactorRiskModel
         The rolling risk model.
 
@@ -81,6 +97,7 @@ def compute_bias_statistics(
         X=X,
         weights=weights,
         rolling_risk_model=rolling_risk_model,
+        forecast_vols=forecast_vols,
     )
     return standardized_returns.rolling(
         rolling_timeframe, min_periods=min_periods
