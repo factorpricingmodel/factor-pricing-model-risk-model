6676fefc7842e41c10ca735afccc241fad0d1d6e
Gavin Chan <gavincyi@gmail.com>
2023-08-19 23:48:34 +0800

    fix: revert forcing numpy array conversion

--- src/fpm_risk_model/risk_model.py
+++ src/fpm_risk_model/risk_model.py
@@ -112,9 +112,9 @@ class RiskModel(ABC):
         if values is None:
             return values
         elif isinstance(values, (DataFrame, Series)):
-            return np.array(values.values)
+            return values.values
         elif isinstance(values, ndarray):
-            return np.array(values)
+            return values
         else:
             raise TypeError(
                 "Expect either pandas DataFrame or numpy array, "
